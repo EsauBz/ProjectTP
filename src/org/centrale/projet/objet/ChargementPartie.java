bc9ec7e6a420fccfc66a4e77aa2b407cd3aa38a3
guiishida <guiishida1357@gmail.com>
2018-10-19 14:37:01 +0200

    Creation de la classe ChargementPartie

---
+++ src/org/centrale/projet/objet/ChargementPartie.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.centrale.projet.objet;
+
+import java.io.BufferedReader;
+
+/**
+ *
+ * @author guiishida
+ */
+public class ChargementPartie {
+    
+    //attribut nomFichier contenant le nom du fichier à charger
+    private final String nomFichier;
+    
+    //attribut responsable pour faire la lecture du fichier qu'on chargera
+    private BufferedReader fichier;
+    
+    /**
+     * Constructeur de la class ChargementPartie avec un parametre
+     * 
+     *
+     * @param fichierNom de type String
+     */
+    
+    public ChargementPartie(String fichierNom){
+        nomFichier = fichierNom; 
+    }
+    
+    public void chargerPartie(){
+        
+        
+    }
+    
+}
