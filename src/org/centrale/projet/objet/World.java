9ac81f935184e947915d012e60f48b41554e2ef3
guiishida <guiishida1357@gmail.com>
2018-10-22 18:50:17 +0200

    Update src/org/centrale/projet/objet/World.java

--- src/org/centrale/projet/objet/World.java
+++ src/org/centrale/projet/objet/World.java
@@ -136,8 +136,8 @@ public class World {
         if (listJouers != null) {
             Scanner sc = new Scanner(System.in);
             for (Joueur a : listJouers) {
-                System.out.println("Voulez-vous faire un mouvement, se battre ou sauvegarder le jeu?");
-                System.out.println("1)Deplacement 2)Battre 3)Sauvegarder");
+                System.out.println("Voulez-vous faire un mouvement ou se battre?");
+                System.out.println("1)Deplacement 2)Battre");
                 int i = 0;
                 try {
                     i = Integer.parseInt(sc.next());
